598a6b282c00ab29be71afdfecd9cc26a467755e
CodyMCyber <codymay422@gmail.com>
2025-04-29 14:51:01 -0400

    Add files via upload
    4/29/25

--- src/components/Drink/Drink.jsx
+++ src/components/Drink/Drink.jsx
@@ -1,13 +1,15 @@
 import styles from "./Drink.module.css";
 import 'bootstrap/dist/css/bootstrap.css';
 
+
 export default function Drink(props) {
   return (<div className={styles.drink}>
-    <h6> id: {props.id}</h6>
-    <h6>Drink name: {props.name}</h6>
-    <h6>Book price: ${props.price}</h6>
-    <h6>type: {props.type}</h6>
-    <h6>calories: {props.calories}</h6>
-    <h6>inStock: {props.inStock}</h6>
+    <div className={styles.id}>id: {props.id}</div>
+    <div className={styles.name}>Drink name: {props.name}</div>
+    <div className={styles.price}>Book price: ${props.price}</div>
+    <div className={styles.type}>type: {props.type}</div>
+    <div className={styles.calories}>calories: {props.calories}</div>
+    <div className={styles.inStock}>inStock: {props.inStock}</div>
+    <button onClick={ () => props.addHandler(props.id) }>Add</button>
   </div>);
 }
--- src/components/DrinkList/DrinkList.jsx
+++ src/components/DrinkList/DrinkList.jsx
@@ -1,7 +1,35 @@
 import storefrontList from '../../assets/storefrontList.json'
 import Drink from '../Drink/Drink';
+import { useState } from 'react';
 
 export default function DrinkList() {
+
+    const [drinks, setDrinks] = useState(storefrontList); // this should be inside the DrinkList component
+
+    const addDrink = (idToBeAdded) => {        
+        const remainingDrinks = drinks.filter(
+            (drink) => {
+                return drink.id == idToBeAdded;
+            }
+        )
+        // update drinks variable
+        setDrinks(remainingDrinks);
+    }
+
+    // To save shopping cart items when cartItems is updated
+useEffect(() => {
+    localStorage.setItem('shoppingCart', JSON.stringify(cartItems));
+  }, [cartItems]);
+  
+  // to load shopping cart iterms
+  const [cartItems, setCartItems] = useState([]);
+  
+  useEffect(() => {
+    const storedCart = localStorage.getItem('shoppingCart');
+    if (storedCart) {
+      setCartItems(JSON.parse(storedCart));
+    }
+  }, []);
     
     return (
         <div className="drink_list">
@@ -9,7 +37,7 @@ export default function DrinkList() {
             {
                 storefrontList.map(drink => (
                     <Drink id={drink.id} name={drink.name} price={drink.price} type={drink.type} 
-                    calories={drink.calories} inStock={drink.inStock} key={drink.id}/>
+                    calories={drink.calories} inStock={drink.inStock} key={drink.id} addHandler={addDrink}/>
                 ))
             }
         </div>
